b397db75a9bf48e5d6116c44bc282dafe2d2d753
ShauliTaragin <shaulitaragin@gmail.com>
2021-12-19 12:57:37 +0200

    changed test

---
+++ src/TestDiGraph.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+from DiGraph import DiGraph
+
+
+class TestDiGraph(TestCase):
+    graph = DiGraph("../data/A0.json")
+    graph1 = DiGraph("../data/A1.json")
+    def test_v_size(self):
+        self.fail()
+
+    def test_e_size(self):
+        self.fail()
+
+    def test_get_all_v(self):
+        self.fail()
+
+    def test_all_in_edges_of_node(self):
+        self.fail()
+
+    def test_all_out_edges_of_node(self):
+        self.fail()
+
+    def test_get_mc(self):
+        self.fail()
+
+    def test_add_edge(self):
+        self.fail()
+
+    def test_add_node(self):
+        self.fail()
+
+    def test_remove_node(self):
+        self.fail()
+
+    def test_remove_edge(self):
+        self.fail()
