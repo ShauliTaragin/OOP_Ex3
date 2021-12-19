3f8ab32f6c5b85921b17b90538f3a56fcd050e8c
ShauliTaragin <shaulitaragin@gmail.com>
2021-12-19 14:58:13 +0200

    created graph algo instance

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,1 +1,35 @@
+from abc import ABC
+from typing import List
+
+from DiGraph import DiGraph
+from GraphAlgoInterface import GraphAlgoInterface
 from src.GraphInterface import GraphInterface
+
+
+class GraphAlgo(GraphAlgoInterface, ABC):
+    def __init__(self, file: str):
+        self.graph = self.load_from_json(file)
+
+
+    def get_graph(self) -> GraphInterface:
+        return self.graph
+
+    def load_from_json(self, file_name: str) -> bool:
+        self.graph = DiGraph(file_name)
+
+    def save_to_json(self, file_name: str) -> bool:
+        pass
+
+    def shortest_path(self, id1: int, id2: int) -> (float, list):
+        pass
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        super().TSP(node_lst)
+
+    def centerPoint(self) -> (int, float):
+        super().centerPoint()
+
+    def plot_graph(self) -> None:
+        pass
+
+
---
+++ src/HelperClass.py
@@ -0,0 +1,15 @@
+from DiGraph import DiGraph
+
+"""
+ * regular bfs using tag of each node to mark whether the node as been visited or not
+     *
+     * 0 -> not_visited
+     * 1 -> visited
+     *
+     *
+"""
+
+
+def bfs(graph: DiGraph) -> bool:
+
+    return True
