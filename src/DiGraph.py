1b9486ad5150b376be1ca2b56f016dc05c644240
Ido Bar <ido1403@gmail.com>
2021-12-22 17:00:16 +0200

    initial changes

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,4 +1,6 @@
-import GraphAlgo
+import random
+import sys
+
 from GraphInterface import GraphInterface
 from Node import Node
 import json
@@ -11,14 +13,14 @@ class DiGraph(GraphInterface):
         self._numOfEdges = 0
         self._mc = 0
         if jsonfile is not None:
-            with open(jsonfile,'r') as jsonFile:
+            with open(jsonfile, 'r') as jsonFile:
                 json_object = json.load(jsonFile)
                 jsonFile.close()
             edges_json = json_object['Edges']
             nodes_json = json_object['Nodes']
             for nodeIter in nodes_json:
-                if('pos' in nodeIter):
-                    location=str(nodeIter['pos']).split(',')
+                if ('pos' in nodeIter):
+                    location = str(nodeIter['pos']).split(',')
                     pos_tuple = (float(location[0]), float(location[1]), float(location[2]))
                     self.add_node(nodeIter['id'], pos_tuple)
                 else:
@@ -29,9 +31,9 @@ class DiGraph(GraphInterface):
                 weight = edgeIter['w']
                 dest = edgeIter['dest']
                 self.add_edge(src, dest, weight)
-            dict_min_max=GraphAlgo.caclulate_minmax(self)[0]
-            for i in
-
+            self.set_location()
+        else:
+            self.set_location()
 
     def v_size(self) -> int:
         return len(self.nodes)
@@ -57,6 +59,7 @@ class DiGraph(GraphInterface):
         if id1 not in self.nodes:
             return {}
         return self.nodes[id1].inEdges
+
     """return a dictionary of all the nodes connected to (into) node_id ,
                     each node is represented using a pair (other_node_id, weight)
                      """
@@ -97,7 +100,7 @@ class DiGraph(GraphInterface):
         Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
         """
 
-    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+    def add_node(self, node_id: int, pos: tuple = (0, 0, 0)) -> bool:
         node_src = self.nodes.get(node_id)
         if node_src is not None:
             return False
@@ -121,9 +124,9 @@ class DiGraph(GraphInterface):
             out_edges = self.nodes[node_id].outEdges.copy()
             in_edges = self.nodes[node_id].inEdges.copy()
             for node_key in out_edges.keys():
-                self.remove_edge(node_id,node_key)
+                self.remove_edge(node_id, node_key)
             for node_key in in_edges.keys():
-                self.remove_edge(node_key,node_id)
+                self.remove_edge(node_key, node_id)
             self.nodes.pop(node_id)
             self._mc += 1
             return True
@@ -166,8 +169,44 @@ class DiGraph(GraphInterface):
         @return: True if the edge was removed successfully, False o.w.
         Note: If such an edge does not exists the function will do nothing
         """
+
     def __repr__(self):
-        return "Graph: |V|={} , |E|={}".format(len(self.nodes),self._numOfEdges)
-    # def clone(self):
+        return "Graph: |V|={} , |E|={}".format(len(self.nodes), self._numOfEdges)
 
+    def set_location(self):
+        dict_min_max = caclulate_minmax(self)[0]
+        for i in self.nodes.values():
+            if i.geolocation == (0, 0, 0):
+                if (dict_min_max[0] != sys.maxsize and dict_min_max[1] != sys.maxsize
+                        and dict_min_max[2] != -sys.maxsize and dict_min_max[3] != -sys.maxsize):
+                    i.geolocation = (random.uniform(dict_min_max[0], dict_min_max[2]),
+                                     random.uniform(dict_min_max[1], dict_min_max[3]), 0)
+                else:
+                    i.geolocation = (random.uniform(0, 9), random.uniform(0, 9), 0 )
 
+def caclulate_minmax(graph: DiGraph) -> (tuple, tuple, tuple):
+
+    minx = sys.maxsize
+    miny = sys.maxsize
+    maxx = (0-sys.maxsize)
+    maxy = (0-sys.maxsize)
+    node_iter = graph.get_all_v()
+    for node in node_iter:
+        if(graph.nodes[node].geolocation != (0, 0, 0)):
+            if graph.nodes[node].geolocation[0] < minx:
+                minx = graph.nodes[node].geolocation[0]
+            if graph.nodes[node].geolocation[0] > maxx:
+                maxx = graph.nodes[node].geolocation[0]
+            if graph.nodes[node].geolocation[1] < miny:
+                miny = graph.nodes[node].geolocation[1]
+            if graph.nodes[node].geolocation[1] > maxy:
+                maxy = graph.nodes[node].geolocation[1]
+    if (minx != sys.maxsize and miny != sys.maxsize and maxx != (0-sys.maxsize) and maxy != (0-sys.maxsize)
+            and graph.nodes[node].geolocation != (0, 0, 0)):
+        absx = abs(minx - maxx)
+        absy = abs(miny - maxy)
+        scale_lon = ((800 - 100) / absx)
+        scale_lat = ((800 - 100) / absy)
+        return (minx, miny, maxx, maxx), (absx, absy), (scale_lon, scale_lat)
+    else:
+        return (minx, miny, maxx, maxx), (0, 0), (0, 0)
